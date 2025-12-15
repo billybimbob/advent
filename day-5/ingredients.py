c93bf825219e3fddba0e77b5d1e97ef965ec902c
billybimbob <noahluvsmilkduds@gmail.com>
2025-12-15 01:59:26 -0600

    add day 5-6

---
+++ day-5/ingredients.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python3
+
+from typing import NamedTuple
+from argparse import ArgumentParser
+import re
+
+class FreshRange(NamedTuple):
+    start: int
+    end: int
+
+    def in_range(self, ingredient: int) -> bool:
+        return self.start <= ingredient <= self.end
+
+
+def total_possible_fresh(fresh_ranges: list[FreshRange]) -> int:
+    ranges_by_start = sorted(fresh_ranges, key=lambda r: r.start)
+    dedupe_ranges = list[FreshRange]()
+
+    for fresh_range in ranges_by_start:
+        prior_range = dedupe_ranges[-1] if dedupe_ranges else None
+
+        if not prior_range:
+            dedupe_ranges.append(fresh_range)
+            continue
+
+        start, end = fresh_range
+
+        if not prior_range.in_range(start) and prior_range.start < start:
+            dedupe_ranges.append(fresh_range)
+            continue
+
+        if not prior_range.in_range(end):
+            dedupe_ranges.append(FreshRange(prior_range.end + 1, end))
+            continue
+
+    return sum(f.end - f.start + 1 for f in dedupe_ranges if f.start <= f.end)
+
+
+def count_fresh_ingredients(ingredients_file: str, total_fresh: bool) -> int:
+    fresh_count = 0
+
+    with open (ingredients_file, 'r') as f:
+        fresh_ranges = list[FreshRange]()
+        is_checking_freshness = False
+
+        for line in f:
+            if not line.strip():
+                is_checking_freshness = True
+                continue
+
+            if not is_checking_freshness:
+                match = re.match(r'^(\d+)-(\d+)$', line)
+                if match:
+                    new_range = FreshRange(int(match.group(1)), int(match.group(2)))
+                    fresh_ranges.append(new_range)
+                continue
+
+            ingredient = int(line.strip())
+            if any(f.in_range(ingredient) for f in fresh_ranges):
+                fresh_count += 1
+
+    if total_fresh:
+        return total_possible_fresh(fresh_ranges)
+
+    return fresh_count
+
+            
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('ingredients_file')
+    parser.add_argument('-t', '--total',  action='store_true', dest='total_fresh')
+    args = parser.parse_args()
+
+    fresh_count = count_fresh_ingredients(args.ingredients_file, args.total_fresh)
+    print(fresh_count)
---
+++ day-6/homework.py
@@ -0,0 +1,101 @@
+#! /usr/bin/env python3
+
+from argparse import ArgumentParser
+from collections import defaultdict
+from collections.abc import Iterable
+import re
+
+def product(values: Iterable[int]) -> int:
+    result = 1
+    for v in values:
+        result *= v
+    return result
+
+def parse_column(column: list[str]) -> int:
+    return int(''.join(column))
+
+
+def total_transposed_values(agg_line: str, values: list[list[str]]) -> int:
+    total = 0
+    column_width = 0
+    operation = ''
+
+    for i, letter in enumerate(agg_line):
+        if letter.isspace():
+            column_width += 1
+            continue
+
+        if operation:
+            target_columns = values[i-1-column_width:i-1]
+
+            if operation == '+':
+                total += sum(parse_column(num_str) for num_str in target_columns)
+            elif operation == '*':
+                total += product(parse_column(num_str) for num_str in target_columns)
+
+        column_width = 0
+        operation = letter
+
+    if operation:
+        target_columns = values[len(agg_line)-1-column_width:len(agg_line)]
+
+        if operation == '+':
+            total += sum(parse_column(num_str) for num_str in target_columns)
+        elif operation == '*':
+            total += product(parse_column(num_str) for num_str in target_columns)
+
+    return total
+
+def solve_ceph_problem(homework_file: str) -> int:
+    total_answer = 0
+
+    with open(homework_file, 'r') as f:
+        transposed_values = list[list[str]]()
+        for line in f:
+            if not line:
+                continue
+
+            line = line.strip('\n')
+
+            if line[0] == '*' or line[0] == '+':
+                total_answer = total_transposed_values(line, transposed_values)
+                continue
+
+            if not transposed_values:
+                for _ in range(len(line)):
+                    transposed_values.append([])
+
+            if len(line) != len(transposed_values):
+                continue
+
+            for i, letter in enumerate(line):
+                transposed_values[i].append(letter)
+
+    return total_answer
+
+
+def solve_homework_problem(homework_file: str) -> int:
+    total_answer = 0
+
+    with open(homework_file, 'r') as f:
+        columns = defaultdict[int, list[int]](list)
+        for line in f:
+            for i, value in enumerate(line.split()):
+                if value == '+':
+                    total_answer += sum(v for v in columns[i])
+                elif value == '*':
+                    total_answer += product(v for v in columns[i])
+                else:
+                    columns[i].append(int(value))
+
+    return total_answer
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('homework_file')
+    parser.add_argument('-t', '--transpose', action='store_true')
+    args = parser.parse_args()
+
+    answer = solve_ceph_problem(args.homework_file) if args.transpose else solve_homework_problem(args.homework_file)
+    print(answer)
+
