c921af4295541aa232a48833a9f47253b662fe1c
billybimbob <noahluvsmilkduds@gmail.com>
2025-12-20 23:51:42 -0600

    day 8 part 2

--- 2025/day-8/circuit.py
+++ 2025/day-8/circuit.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 from collections.abc import Iterable
-from typing import NamedTuple, Union
+from typing import NamedTuple, Optional
 from argparse import ArgumentParser
 import re
 import math
@@ -17,6 +17,11 @@ class PositionPair(NamedTuple):
     a: Position
     b: Position
     distance: float
+
+
+class ConnectionMap(NamedTuple):
+    connections: list[set[Position]]
+    last_pair: Optional[tuple[Position, Position]]
 
 
 def product(values: Iterable[int], start=1) -> int:
@@ -42,26 +47,35 @@ def read_junction_boxes(junctions_file: str) -> list[Position]:
 
 
 def wire_junction_boxes(
-    junction_boxes: list[Position], num_connections: int
-) -> list[set[Position]]:
-    connections = list[set[Position]]()
+    junction_boxes: list[Position], num_connections: Optional[int]
+) -> ConnectionMap:
     pairs = list[PositionPair]()
-
     for i, a in enumerate(junction_boxes):
         for b in junction_boxes[i:]:
             distance = math.dist(a, b)
             if distance != 0:
                 pairs.append(PositionPair(a, b, distance))
 
-    pairs.sort(key=lambda p: p.distance, reverse=True)
+    pairs.sort(key=lambda p: p.distance)
+    if num_connections:
+        pairs = pairs[:num_connections]
 
-    for i in range(num_connections):
-        if not pairs:
-            break
+    remaining_boxes = set(junction_boxes)
+    i = 0
 
-        a, b, _ = pairs.pop()
-        target_a: Union[set[Position], None] = None
-        target_b: Union[set[Position], None] = None
+    connections = list[set[Position]]()
+    last_pair: Optional[PositionPair] = None
+
+    while i < len(pairs) and (remaining_boxes or len(connections) > 1):
+        pair = pairs[i]
+        a, b, _ = pair
+
+        if a not in remaining_boxes and b not in remaining_boxes:
+            i += 1
+            continue
+
+        target_a: Optional[set[Position]] = None
+        target_b: Optional[set[Position]] = None
 
         for circuit in connections:
             if a in circuit:
@@ -70,7 +84,7 @@ def wire_junction_boxes(
             if b in circuit:
                 target_a = circuit
 
-        if target_a and target_b and target_a != target_b:
+        if target_a and target_b and target_a:
             target_a.update(target_b)
             connections.remove(target_b)
 
@@ -83,24 +97,35 @@ def wire_junction_boxes(
         else:
             connections.append({a, b})
 
-    connected_boxes = {p for c in connections for p in c}
-    connections.extend({b} for b in junction_boxes if b not in connected_boxes)
+        remaining_boxes.discard(a)
+        remaining_boxes.discard(b)
 
-    return connections
+        last_pair = pair
+        i += 1
+
+    connections.extend({b} for b in remaining_boxes)
+
+    return ConnectionMap(connections, last_pair)
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("junctions_file")
-    parser.add_argument("-c", "--connections", type=int, default=1000)
-    parser.add_argument("-t", "--top", type=int, default=3)
+    parser.add_argument("-c", "--connections", type=int)
+    parser.add_argument("-t", "--top", type=int)
 
     args = parser.parse_args()
 
     boxes = read_junction_boxes(args.junctions_file)
-    connections = wire_junction_boxes(boxes, args.connections)
+    connections, last_pair = wire_junction_boxes(boxes, args.connections)
 
-    connection_lens = [len(c) for c in connections]
-    answer = product(sorted(connection_lens, reverse=True)[: args.top])
+    if args.top:
+        connection_lens = [len(c) for c in connections]
+        answer = product(sorted(connection_lens, reverse=True)[: args.top])
+    elif last_pair:
+        a, b, _ = last_pair
+        answer = a.x * b.x
+    else:
+        answer = 0
 
     print(answer)
